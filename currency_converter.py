b58e05e9e3e605a6c3edc7992ef07f2b1a129334
kapmar <98036617+kapmar@users.noreply.github.com>
2022-01-19 17:19:07 +0100

    comments removal/variable language changed

--- currency_converter.py
+++ currency_converter.py
@@ -1,33 +1,26 @@
-#ftiakse ena programma to opoio tha exei ena function me onoma convert_currency to opoio tha dexetai 2 arguments
-# to proto argument tha nai to poso twn eurw kai to deutero to nomisma pou theleis na ginetai i allagi
-#to function tha elegxei ean o xristis edwse 1 apo ta 3 nomismata UK USD,YEN, kai tha kanei tis antistoixes metatropes
-# se alli periptosi tha anaferei oti to sygkekrimeno nomisma den upostirizetai.
-#telos meta tin metatropi tha rotaei ton xristh ean thelei na kanei alli metatropi kai ean oxi tha termatizei
-#alliws tha ksana zita apto xristi na dwsei poso kai nomisma
-
-def convert_currency(euro, metatropi):
-    if metatropi == "uk":
+def convert_currency(euro, conversion):
+    if conversion == "uk":
         print(euro / 1.25)
-    elif metatropi == "usd":
+    elif conversion == "usd":
         print(euro / 0.95)
-    elif metatropi == "yen":
+    elif conversion == "yen":
         print(euro / 0.46)
     else:
-        print("to nomisma den upostirizetai")
+        print("currency not supported")
 
 x = 0
 while x == 0:
-    poso = input("posa thelete na metatrepsete?: ")
-    nomisma = input("se ti nomisma tha thelate na metatrepsete?(uk/usd/yen): ")
-    convert_currency(float(poso), nomisma)
+    amount = input("how much money are you willing to convert: ")
+    currency = input("in which currency are you willing to convert?(uk/usd/yen): ")
+    convert_currency(float(amount), currency)
     y = 0
     while y == 0: 
-        pisw_eksodos = input("thelete na kanete allh metatroph?(nai/oxi): ")
-        if pisw_eksodos == "oxi" or "OXI":
+        back_exit = input("would you like to make another conversion?(yes/no): ")
+        if back_exit == "no" or "NO":
             y = 1
             x = 1
-        elif pisw_eksodos == "nai" or "NAI":
+        elif back_exit == "yes" or "YES":
             y = 1
         else:
-            print("NAI H OXI RE MALAKA")
+            print("would you like to make another conversion?(yes/no)")
     
